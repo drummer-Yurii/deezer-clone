b8a5e826cb47366e1bd348848490a1737cad8983
Yurii Rybachok <drummer2009@ukr.net>
2023-04-10 21:42:42 +0300

    side menu item component & assets

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,10 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
+import { RouterLink, RouterView } from 'vue-router';
 
 import Magnify from 'vue-material-design-icons/Magnify.vue';
 import Bell from 'vue-material-design-icons/Bell.vue';
+
+import SideMenuItem from './components/SideMenuItem.vue';
 
 </script>
 
@@ -44,14 +46,17 @@ import Bell from 'vue-material-design-icons/Bell.vue';
       class="fixed w-[240px] bg-[#191922] h-[100vh] border-r border-r-[#32323D]"
     >
       <div class="w-full pl-6 pt-3 cursor-pointer">
-        <RouterLink to="/" class="text-white">
-          Deezer
+        <RouterLink to="/">
+          <img width="130" src="/images/deezer-logo.png">
         </RouterLink>
       </div>
 
       <div class="mt-[53px]"></div>
 
-      //MenuSection
+      <SideMenuItem iconStrin="music" :iconSize="20" pageUrl="/" name="Music" />
+      <SideMenuItem iconStrin="podcast" :iconSize="20" pageUrl="/podcasts" name="Podcasts" />
+      <SideMenuItem iconStrin="explore" :iconSize="20" pageUrl="/artist" name="Explore" />
+      <SideMenuItem iconStrin="favourite" :iconSize="20" pageUrl="/favourite" name="Favourites" />
 
       <div class="text-[#A2A2AD] font-light text-[12px] pl-[62px] mt-[25px]">
         <div class="py-[9px] hover:text-[#EF5465] cursor-pointer">Favorite tracks</div>
@@ -63,7 +68,7 @@ import Bell from 'vue-material-design-icons/Bell.vue';
     </div>
 
     <div class="fixed w-[calc(100%-240px)] h-[calc(100%-56px)] ml-[240px] mt-[56px] overflow-x-auto">
-      <!-- <RouterView /> -->
+      <RouterView />
     </div>
 </template>
 
