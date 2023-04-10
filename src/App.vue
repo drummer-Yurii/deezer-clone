1455b3382d884f296a083236886f2b50dc817024
Yurii Rybachok <drummer2009@ukr.net>
2023-04-10 19:52:58 +0300

    top nav section

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,44 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
+
+import Magnify from 'vue-material-design-icons/Magnify.vue';
+import Bell from 'vue-material-design-icons/Bell.vue';
+
 </script>
 
 <template>
-    <h1 class="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  <RouterView />
+    <div
+      id="TopNav"
+      class="fixed right-0 flex items-center justify-between w-[calc(100%-240px)] h-[56px] border-b border-b-[#32323D]"
+    >
+      <div class="flex items-center w-full">
+        <Magnify class="pl-6 mt-1 pr-2" fillColor="#7E7E88" :size="22" />
+        <input 
+          class="
+            p-1
+            bg-transparent
+            outline-none
+            font-[300]
+            placeholder-[#BEBEC7]
+            text-[#FFFFFF]
+            w-full
+            max-w-xl
+          "
+          placeholder="Search"
+          type="text"
+        >
+      </div>
+      <div class="flex items-center pr-10">
+        <div class="mr-4 p-1 hover:bg-gray-600 rounded-full cursor-pointer">
+          <Bell fillColor="#FFFFFF" :size="20" />
+        </div>
+        <img 
+          class="rounded-full w-[33px]"
+          src="https://avatars.githubusercontent.com/u/104525685?v=4"
+        >
+      </div>
+    </div>
+  <!-- <RouterView /> -->
 </template>
 
 
