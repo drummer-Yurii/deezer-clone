a15231c530feef58fb3b3f3761e67ed2b0bb18cd
Yurii Rybachok <drummer2009@ukr.net>
2023-04-11 11:08:07 +0300

    MixesInspiredBy component

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,5 @@
 <script setup>
-
+import MixesInspiredBy from '../components/MixesInspiredBy.vue';
 </script>
 
 <template>
@@ -13,21 +13,87 @@
       <div class="py-3"></div>
 
       <div class="flex justify-between w-full gap-8">
-        <div class="xl:w-1/3 w-1/2 text-white">
-          <div>TEST</div>
-          <div>TEST</div>
-          <div>TEST</div>
+        <div class="xl:w-1/3 w-1/2">
+          <MixesInspiredBy 
+            class="pb-1.5"
+            text="VAMPIRE (feat. Oli Sykes of Bring Me The Horizon)"
+            by="Ic3peak"
+            to="/artist"
+            image="https://picsum.photos/id/147/300/300"
+          />
+          <MixesInspiredBy 
+            class="pb-1.5"
+            text="Deutchland"
+            by="Rammstein"
+            to="/artist"
+            image="https://picsum.photos/id/166/300/300"
+          />
+          <MixesInspiredBy 
+            class="pb-1.5"
+            text="Fallout"
+            by="Masked Wolf"
+            to="/artist"
+            image="https://picsum.photos/id/127/300/300"
+          />
         </div>
-        <div class="xl:w-1/3 w-1/2 text-white">
-          <div>TEST</div>
-          <div>TEST</div>
-          <div>TEST</div>
+         <div class="xl:w-1/3 w-1/2">
+          <MixesInspiredBy 
+            class="pb-1.5"
+            text="Pray For Plagues"
+            by="Bring Me the Horizon" 
+            to="/artist"
+            image="https://picsum.photos/id/201/300/300"
+          />
+          <MixesInspiredBy 
+            class="pb-1.5"
+            text="Candy Truck / You Expected"
+            by="Bring Me the Horizon" 
+            to="/artist"
+            image="https://picsum.photos/id/222/300/300"
+          />
+          <MixesInspiredBy 
+            class="pb-1.5"
+            text="L'efant sauvage"
+            by="Gojira" 
+            to="/artist"
+            image="https://picsum.photos/id/212/300/300"
+          />
         </div>
-        <div class="xl:w-1/3 w-1/2 text-white">
-          <div>TEST</div>
-          <div>TEST</div>
-          <div>TEST</div>
+        <div class="xl:block hidden xl:w-1/3">
+          <MixesInspiredBy 
+            class="pb-1.5"
+            text="Bad Life"
+            by="Sigrid" 
+            to="/artist"
+            image="https://picsum.photos/id/211/300/300"
+          />
+          <MixesInspiredBy 
+            class="pb-1.5"
+            text="Dystopia"
+            by="Megadeth" 
+            to="/artist"
+            image="https://picsum.photos/id/123/300/300"
+          />
+          <MixesInspiredBy 
+            class="pb-1.5"
+            text="Can You Feel My Heart"
+            by="Bring Me the Horizon" 
+            to="/artist"
+            image="https://picsum.photos/id/94/300/300"
+          />
         </div>
+      </div>
+    </div>
+
+    <div class="px-8 mt-8 min-w-[800px]">
+      <div class="text-white text-xl font-semibold inline-block">
+        Made for you
+      </div>
+
+      <div class="py-3"></div>
+
+      <div class="flex justify-start gap-7">
+        
       </div>
     </div>
   </div>
