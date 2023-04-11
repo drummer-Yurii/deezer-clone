e05328e843714aa33e6ea0a6cb53081cb1897ab7
Yurii Rybachok <drummer2009@ukr.net>
2023-04-11 20:50:32 +0300

    create ArtistView

---
+++ src/views/ArtistView.vue
@@ -0,0 +1,15 @@
+<script setup>
+import Magnify from 'vue-material-design-icons/Magnify.vue';
+import Play from 'vue-material-design-icons/Play.vue';
+import Pause from 'vue-material-design-icons/Pause.vue';
+import DotsHorizontal from 'vue-material-design-icons/DotsHorizontal.vue';
+import HeartOutline from 'vue-material-design-icons/HeartOutline.vue';
+import ClockTimeFiveOutline from 'vue-material-design-icons/ClockTimeFiveOutline.vue';
+import artist from '../artist.json';
+</script>
+
+<template>
+  <div id="HeaderSection" class="max-w-[1500px] mx-auto">
+    
+  </div>
+</template>
