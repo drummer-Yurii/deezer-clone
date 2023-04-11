3ee1d41fdc103db0326e417531c4feed1bef0920
Yurii Rybachok <drummer2009@ukr.net>
2023-04-11 21:17:58 +0300

    add router for artist view

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import ArtistView from '../views/ArtistView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -8,7 +9,11 @@ const router = createRouter({
       path: '/',
       name: 'home',
       component: HomeView
-    }
+    },
+    {
+      path: '/artist',
+      component: ArtistView
+    },
   ]
 })
 
