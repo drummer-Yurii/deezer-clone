ab45f72392d50fc77ba9dbd36faff7a8890bdd55
Yurii Rybachok <drummer2009@ukr.net>
2023-04-13 13:16:19 +0300

    lyricRow component

---
+++ src/components/LyricRow.vue
@@ -0,0 +1,34 @@
+<script setup>
+import { toRefs, watch } from 'vue';
+import lyrics from '../lyrics.json';
+import GitarElectric from 'vue-material-design-icons/GuitarElectric.vue';
+
+import { useSongStore } from '../stores/song';
+import { storeToRefs } from 'pinia';
+const useSong = useSongStore();
+const { trackTime, currentTrack, lyricsPosition } = storeToRefs(useSong);
+
+const props = defineProps({ time:String, words: String })
+const { time, words } = toRefs(props)
+
+watch(() => trackTime.value, (trackTime) => {
+    setTimeout(() => {
+        lyrics[currentTrack.value.id].forEach(res => {
+            if (res.time == trackTime) lyricsPosition.value = res.time
+        })
+    }, 1000)
+})
+</script>
+
+<template>
+    <div
+        id="time"
+        class="my-10 text-white opacity-100"
+        :class="{'opacity-30': lyricsPosition !== time}"
+    >
+        <div v-if="words === 'INSTRAMENTAL SECTION'" class="flex w-full">
+            <GitarElectric :size="200" class="mx-auto" />
+        </div>
+        <div v-if="words !== 'INSTRAMENTAL SECTION'">{{ words }}</div>
+    </div>
+</template>
